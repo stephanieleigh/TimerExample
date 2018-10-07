5ae85bfce79714d686963fcc069d00777cb397da
stephanieleigh <36084964+stephanieleigh@users.noreply.github.com>
2018-10-07 12:42:32 -0500

    add TimerExample to repo

--- TimerExample/Program.cs
+++ TimerExample/Program.cs
@@ -1,8 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿// Advanced C#
+// Module 07
+// Timer Example from MVA
+// Stephanie Wilson
+// October 5, 2018
+
+using System;
+using static System.Console;
+using System.Timers;
 
 namespace TimerExample
 {
@@ -10,6 +14,32 @@ namespace TimerExample
     {
         static void Main(string[] args)
         {
+            Timer myTimer = new Timer(2000);
+
+            myTimer.Elapsed += MyTimer_Elapsed;
+            myTimer.Elapsed += MyTimer_Elapsed1;
+
+            myTimer.Start();
+
+            WriteLine("Press enter to remove the red event.");
+
+            ReadLine();
+
+            myTimer.Elapsed -= MyTimer_Elapsed1;
+
+            ReadLine();
+        }
+
+        private static void MyTimer_Elapsed1(object sender, ElapsedEventArgs e)
+        {
+            ForegroundColor = ConsoleColor.Red;
+            WriteLine("Elapsed1: {0:HH:mm:ss.fff}", e.SignalTime);
+        }
+
+        private static void MyTimer_Elapsed(object sender, ElapsedEventArgs e)
+        {
+            ForegroundColor = ConsoleColor.White;
+            WriteLine("Elapsed: {0:HH:mm:ss.fff}", e.SignalTime);
         }
     }
 }
